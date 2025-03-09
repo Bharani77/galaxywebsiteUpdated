1b51ddf092d083c44c71510103bcc30fc3960358
Bharani <bharani007dharan@gmail.com>
2025-03-09 23:00:02 +0530

    updated domain

--- src/app/profile/GalaxyForm.tsx
+++ src/app/profile/GalaxyForm.tsx
@@ -219,7 +219,7 @@ const GalaxyForm: React.FC = () => {
         return acc;
       }, {} as Record<string, string>);
   
-      const response = await fetch(`https://bharanidharan.serveo.net/${action}/${formNumber}`, {
+      const response = await fetch(`https://jlcgrk-32774.csb.app/${action}/${formNumber}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
